2a4c7af6cc08220fa534a8002755f882a14e4be8
syedifelse <syedifelse@gmail.com>
2019-09-15 00:32:00 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Syed Ibrahim Faraz",
+    "role": "Frontend Developer",
+    "linkedinId":" 8123451053",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, Discover new fragrances in my free time!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url": "https://www.linkedin.com/in/syed-if-485a59174/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/syedifelse",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url": "https://twitter.com/_Syedif",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Ghousia college of Engineering and pursuing my B.E from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Channapatna ,Karnataka,India",
+    "website":"https://sif.github.io",
+    "education":[
+      {
+        "UniversityName": "The Ghousia college of Engineering ",
+        "specialization":"B.E in C.S.E",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+    
+    ],
+   /* "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],*/
+    "skillsDescription":"My skills are",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+   /* "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]*/
+  }
+  
+  export default resumeData
